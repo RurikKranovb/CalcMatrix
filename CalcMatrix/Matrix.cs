81bf6e4508952445fa75514adb08251d9919d552
bobrov.da <dmr.bobrov@yandex.ru>
2025-05-12 15:05:34 +0300

    Add model matrix

---
+++ CalcMatrix/Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcMatrix
+{
+   public class Matrix
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+
+        private readonly double[,] data;
+    }
+}
