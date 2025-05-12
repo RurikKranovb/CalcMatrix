83d2cbf25f2dab0f4ffa557cdca7699e386792d1
bobrov.da <dmr.bobrov@yandex.ru>
2025-05-12 15:06:12 +0300

    add read element matrix

--- CalcMatrix/Program.cs
+++ CalcMatrix/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Matrix a = CreateMatrix("A");
+        }
+
+        private static Matrix CreateMatrix(string matrixName)
+        {
+            Console.WriteLine($"Матрица {matrixName}");
+            int rows = ReadMatrixElement("Количество строк матрицы:");
+            int cols = ReadMatrixElement("Количество столбцов матрицы:");
+
+            return null;
+        }
+
+        private static int ReadMatrixElement(string rowsMatrix)
+        {
+            int value;
+            Console.Write(rowsMatrix);
+
+            while (!int.TryParse(Console.ReadLine(), out value) || value <=0)
+                Console.Write("Ошибка! Введите целое положительное число:");
+
+            return value;
         }
     }
 }
